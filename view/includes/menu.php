1fd3da7aca48ac5209c9ef3186e47fe09ab69189
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2023-05-05 22:00:11 -0300

    Barra de pesquisa adicionada

--- view/includes/menu.php
+++ view/includes/menu.php
@@ -1,7 +1,27 @@
 <div class="header-menu container">
   <div class="row">
     <div class="col-md-3 col-sm-6"><img src="src/images/logo.webp" alt=""></div>
-    <div class="col-md-6">div2</div>
+    <div class="col-md-6  ">
+      <div class="bg-light">
+        <!-- Example single danger button -->
+        <div class="btn-group">
+          <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+            Action
+          </button>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li>
+              <hr class="dropdown-divider">
+            </li>
+            <li><a class="dropdown-item" href="#">Separated link</a></li>
+          </ul>
+        </div>
+        <input type="search">
+      </div>
+    </div>
+
     <div class="col-md-3">div3</div>
   </div>
 </div>
--- view/includes/topo.php
+++ view/includes/topo.php
@@ -1,15 +1,15 @@
 <nav class="topo-nav-links">
         <div class="topo-nav-links container d-flex justify-content-between">
 
-                <div class="mx-1 px-2 ">
+                <div class="mx-1 px-2 d-none d-lg-block">
                         <span class="col-md-4 px-2"><i class="bi bi-telephone-fill text-danger"></i><a href="#"
                                         class=" px-2 nav-links">+55 61 998877666</a></span>
                         <span class="col-md-4 px-2 "><i class="bi bi-envelope text-danger"></i></i><a href="#"
                                         class=" px-2 nav-links">vendafacil@gmail.com</a></span>
                         <span class="col-md-4 px-2 "><i class="bi bi-geo-alt-fill text-danger"></i><a href="#"
-                                        class=" px-2 nav-links">Formosa - Goias,
-                                        Brasil</a></span>
+                                        class=" px-2 nav-links">Formosa - Goias, Brasil</a></span>
                 </div>
+
 
                 <div class="mx-1 px-2">
                         <span class="col-md-6 px-2 "><i class="bi bi-currency-dollar text-danger"></i><a href="#"
