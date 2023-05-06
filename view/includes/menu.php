633c9a8cce5203ed71f239bbd4e601f52127b1fc
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2023-05-06 17:54:25 -0300

    header finalizado e inicio da home

--- view/includes/menu.php
+++ view/includes/menu.php
@@ -1,24 +1,24 @@
 <nav class="header-container container    ">
   <div class="row  my-3">
     <!-- logo -->
-    <figure class="col-md-3 col-sm-6 px-2">
+    <figure class="logo col-md-3 col-sm-6 px-2">
       <img src="src/images/logo.webp" alt="">
     </figure>
-    <div class="search-bar col-md-6 d-flex border rounded-5 bg-light px-2">
+    <div class=" search-bar col-md-6 d-flex  rounded-5  bg-light px-0">
+      <input type="text" class="input input-pesquisar form-control mx-0" name="" id="">
+      <button class="btn btn-pesquisar  ">Pesquisar</button>
 
     </div>
 
-    <div class="col-md-3 text-light border px-4 row">
-      <a href="#" class="nav-links d-flex align-items-center justify-content-center col-5">
+    <div class="col-md-3 text-light  px-4 d-flex justify-content-between row">
+      
+      <a href="#" class="ps-5 nav-links d-flex align-items-center justify-content-center flex-column col-4 text-nowrap">
         <i class="bi bi-heart"></i>
-        <span>Lista de Desejos</span>
+        <span>Favoritos</span>
       </a>
-      <a href="#" class="nav-links d-flex align-items-center justify-content-betweem
-      
-      
-      col-5">
+      <a href="#" class="nav-links d-flex align-items-center justify-content-center  flex-column col-4 text-nowrap">
         <i class="bi bi-cart-fill"></i>
-        <span>Lista de Desejos</span>
+        <span>Carrinho</span>
       </a>
 
 
--- view/secoes/home.php
+++ view/secoes/home.php
@@ -1,1 +1,46 @@
-<h1>Aqui será a página principal</h1>
+<main class="container">
+    <nav class="navbar bg-light">
+        <div class="container ">
+
+
+            <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container-fluid">
+                    
+                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                        <div class="navbar-nav">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                            <a class="nav-link" href="#">Features</a>
+                            <a class="nav-link" href="#">Pricing</a>
+                            <a class="nav-link disabled">Disabled</a>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </nav>
+    <section class="collections row">
+        <div class="col-4 ">
+            <div class="a">
+                <p class="text-collection text-light text-start  fw-bold py-5 px-4 text-wrap " >Coleção laptop</p>
+                <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
+            </div>
+            <figure class=""><img class="img-collections" src="src/images/laptop.webp" alt=""></figure>
+        </div>
+        <div class="col-4 ">
+            <div class="a">
+                <p class="text-collection text-light text-start  fw-bold py-5 px-4 text-wrap " >Coleção Headset</p>
+                <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
+            </div>
+            <figure class=""><img class="img-collections" src="src/images/headset.webp" alt=""></figure>
+        </div>
+        <div class="col-4 ">
+            <div class="a">
+                <p class="text-collection text-light text-start  fw-bold py-5 px-4 text-wrap " >Coleção Câmera</p>
+                <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
+            </div>
+            <figure class=""><img class="img-collections" src="src/images/camera.webp" alt=""></figure>
+        </div>
+        
+        
+    </section>
+</main> 
