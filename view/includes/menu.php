554e9959dc429e5c509a80cbdf7bcb9a538e86b0
Tiago Tavares <tiagobritot2016@gmail.com>
2023-05-06 12:37:12 -0300

    Adicionado a wishlist e carrrinho

--- view/includes/menu.php
+++ view/includes/menu.php
@@ -1,29 +1,36 @@
-<div class="header-menu container">
-  <div class="row">
-    <div class="col-md-3 col-sm-6"><img src="src/images/logo.webp" alt=""></div>
-    <div class="col-md-6  ">
-      <div class="bg-light">
-        <!-- Example single danger button -->
-        <div class="btn-group">
-          <button type="button" class="btn btn-danger dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
-            Action
-          </button>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-            <li>
-              <hr class="dropdown-divider">
-            </li>
-            <li><a class="dropdown-item" href="#">Separated link</a></li>
-          </ul>
-        </div>
-        <input type="search">
-      </div>
+<nav class="header-container container    ">
+  <div class="row  my-3">
+    <!-- logo -->
+    <figure class="col-md-3 col-sm-6 px-2">
+      <img src="src/images/logo.webp" alt="">
+    </figure>
+    <div class="search-bar col-md-6 d-flex border rounded-5 bg-light px-2">
+
     </div>
 
-    <div class="col-md-3">div3</div>
+    <div class="col-md-3 text-light border px-4 row">
+      <a href="#" class="nav-links d-flex align-items-center justify-content-center col-5">
+        <i class="bi bi-heart"></i>
+        <span>Lista de Desejos</span>
+      </a>
+      <a href="#" class="nav-links d-flex align-items-center justify-content-betweem
+      
+      
+      col-5">
+        <i class="bi bi-cart-fill"></i>
+        <span>Lista de Desejos</span>
+      </a>
+
+
+
+
+    </div>
   </div>
+</nav>
+
+
+
+</div>
 </div>
 <!-- <ul>
   <li>
