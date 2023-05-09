e8c9c9ed25bbfbe397276c8733846753f95c41e0
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2023-05-09 15:02:50 -0300

    Barra de menu e sessao de coleção adicionadas

--- view/secoes/home.php
+++ view/secoes/home.php
@@ -1,46 +1,58 @@
 <main class="container">
-    <nav class="navbar bg-light">
-        <div class="container ">
-
-
-            <nav class="navbar navbar-expand-lg bg-light">
-                <div class="container-fluid">
-                    
-                    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                        <div class="navbar-nav">
-                            <a class="nav-link active" aria-current="page" href="#">Home</a>
-                            <a class="nav-link" href="#">Features</a>
-                            <a class="nav-link" href="#">Pricing</a>
-                            <a class="nav-link disabled">Disabled</a>
-                        </div>
+    <nav class="navbar bg-light row my-0 py-0">
+        <nav class="navbar navbar-expand row bg-light col-sm-12 d-flex justify-content-center my-0">
+            <div class="container-fluid  d-flex justify-content-center">
+                <div class="collapse navbar-collapse  border-bottom-3   " id="navbarNavAltMarkup">
+                    <div class="navbar-nav d-flex justify-content-center align-middle">
+                        <a class="nav-link active py-2" href="#">Home</a>
+                        <a class="nav-link active py-2" href="#">Features</a>
+                        <a class="nav-link active py-2" href="#">Features</a>
+                        <a class="nav-link active py-2" href="#">Pricing</a>
+                        <a class="nav-link active py-2" href="#">Home</a>
+                        <a class="nav-link active py-2" href="#">Features</a>
+                        <a class="nav-link active py-2" href="#">Features</a>
+                        <a class="nav-link active py-2" href="#">Pricing</a>
                     </div>
                 </div>
-            </nav>
-        </div>
-    </nav>
-    <section class="collections row">
-        <div class="col-4 ">
-            <div class="a">
-                <p class="text-collection text-light text-start  fw-bold py-5 px-4 text-wrap " >Coleção laptop</p>
-                <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
             </div>
-            <figure class=""><img class="img-collections" src="src/images/laptop.webp" alt=""></figure>
-        </div>
-        <div class="col-4 ">
-            <div class="a">
-                <p class="text-collection text-light text-start  fw-bold py-5 px-4 text-wrap " >Coleção Headset</p>
-                <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
+        </nav>
+        <section class="collections row ">
+
+            <div class="col-md-4 col-sm-12 mx-auto">
+                <div class="d-flex flex-column align-items-center">
+                    <div class="collections-efeito">
+                        <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
+                            laptop</p>
+                        <a href="#" class="links-collection px-4">Compre agora <i
+                                class="bi bi-arrow-right-circle-fill"></i></a>
+                    </div>
+                    <figure><img class="img-collections" src="src/images/laptop.webp" alt=""></figure>
+                </div>
             </div>
-            <figure class=""><img class="img-collections" src="src/images/headset.webp" alt=""></figure>
-        </div>
-        <div class="col-4 ">
-            <div class="a">
-                <p class="text-collection text-light text-start  fw-bold py-5 px-4 text-wrap " >Coleção Câmera</p>
-                <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
+            <div class="col-md-4 col-sm-12 mx-auto">
+                <div class="d-flex flex-column align-items-center">
+                    <div class="collections-efeito">
+                        <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
+                            Headset</p>
+                        <a href="#" class="links-collection px-4">Compre agora <i
+                                class="bi bi-arrow-right-circle-fill"></i></a>
+                    </div>
+                    <figure><img class="img-collections" src="src/images/headset.webp" alt=""></figure>
+                </div>
             </div>
-            <figure class=""><img class="img-collections" src="src/images/camera.webp" alt=""></figure>
-        </div>
-        
-        
-    </section>
-</main> 
+            <div class="col-md-4 col-sm-12 mx-auto collection">
+                <div class="d-flex flex-column align-items-center">
+                    <div class="collections-efeito">
+                        <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
+                            Câmera</p>
+                        <a href="#" class="links-collection px-4">Compre agora <i
+                                class="bi bi-arrow-right-circle-fill"></i></a>
+                    </div>
+                    <figure><img class="img-collections" src="src/images/camera.webp" alt=""></figure>
+                </div>
+            </div>
+
+
+
+        </section>
+</main>
