7bdbdb6332be683e4758bfd3880e5a857d1e7516
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2023-05-09 21:42:18 -0300

    barra com menu adicionada

--- view/secoes/home.php
+++ view/secoes/home.php
@@ -4,21 +4,20 @@
             <div class="container-fluid  d-flex justify-content-center">
                 <div class="collapse navbar-collapse  border-bottom-3   " id="navbarNavAltMarkup">
                     <div class="navbar-nav d-flex justify-content-center align-middle">
-                        <a class="nav-link active py-2" href="#">Home</a>
-                        <a class="nav-link active py-2" href="#">Features</a>
-                        <a class="nav-link active py-2" href="#">Features</a>
-                        <a class="nav-link active py-2" href="#">Pricing</a>
-                        <a class="nav-link active py-2" href="#">Home</a>
-                        <a class="nav-link active py-2" href="#">Features</a>
-                        <a class="nav-link active py-2" href="#">Features</a>
-                        <a class="nav-link active py-2" href="#">Pricing</a>
+                        <a class="nav-link active py-2 flex-wrap" href="#">Home</a>
+                        <a class="nav-link active py-2 flex-wrap" href="#">Ofertas quentes</a>
+                        <a class="nav-link active py-2 flex-wrap" href="#">Categorias</a>
+                        <a class="nav-link active py-2 flex-wrap" href="#">Laptops</a>
+                        <a class="nav-link active py-2 flex-wrap" href="#">Smartphones</a>
+                        <a class="nav-link active py-2 flex-wrap" href="#">Câmeras</a>
+                        <a class="nav-link active py-2 flex-wrap" href="#">Acessórios</a>
                     </div>
                 </div>
             </div>
         </nav>
-        <section class="collections row ">
+        <section class="collections container-fluid row mb-5">
 
-            <div class="col-md-4 col-sm-12 mx-auto">
+            <div class="col-md-6 col-sm-12 col-lg-4 flex-wrap">
                 <div class="d-flex flex-column align-items-center">
                     <div class="collections-efeito">
                         <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
@@ -29,7 +28,7 @@
                     <figure><img class="img-collections" src="src/images/laptop.webp" alt=""></figure>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-12 mx-auto">
+            <div class="col-md-6 col-sm-12 col-lg-4 mx-auto flex-wrap">
                 <div class="d-flex flex-column align-items-center">
                     <div class="collections-efeito">
                         <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
@@ -40,7 +39,7 @@
                     <figure><img class="img-collections" src="src/images/headset.webp" alt=""></figure>
                 </div>
             </div>
-            <div class="col-md-4 col-sm-12 mx-auto collection">
+            <div class="col-md-6  col-sm-12 col-lg-4 collection px-4 flex-wrap">
                 <div class="d-flex flex-column align-items-center">
                     <div class="collections-efeito">
                         <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
@@ -51,8 +50,30 @@
                     <figure><img class="img-collections" src="src/images/camera.webp" alt=""></figure>
                 </div>
             </div>
-
-
-
         </section>
+        <div class="Container">
+            <div class="row border">
+                <h3 class="fs-2  col-sm-12 col-md-8 border">
+                    Novos produtos
+                </h3>
+                <nav class="navbar navbar-expand bg-light col-sm-12 col-md-4 border my-0">
+                    <div class="collapse navbar-collapse" id="navbarNav">
+                        <ul class="navbar-nav border d-flex justify-content-center align-items-center">
+                            <li class="nav-item a">
+                                <a class=" novo-produto-bar" aria-current="page" href="#">Laptops</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link novo-produto-bar active" href="#">Smartphones</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link novo-produto-bar active " href="#">Câmeras</a>
+                            </li>
+                            <li class="nav-item ">
+                                <a class="nav-link novo-produto-bar active ">Acessorios</a>
+                            </li>
+                        </ul>
+                    </div>
+            </div>
+        </div>
+        
 </main>
