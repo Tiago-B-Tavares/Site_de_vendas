e6247c8a5999a3f1fd0624f63816204ce403a3af
Tiago Tavares <tiagobritot2016@gmail.com>
2023-05-10 13:52:33 -0300

    comecei a sessao de destaque

--- view/includes/menu.php
+++ view/includes/menu.php
@@ -7,11 +7,8 @@
     <div class=" search-bar col-md-6 d-flex  rounded-5  bg-light px-0">
       <input type="text" class="input input-pesquisar form-control mx-0" name="" id="">
       <button class="btn btn-pesquisar  ">Pesquisar</button>
-
     </div>
-
     <div class="col-md-3 text-light  px-4 d-flex justify-content-between row">
-      
       <a href="#" class="ps-5 nav-links d-flex align-items-center justify-content-center flex-column col-4 text-nowrap">
         <i class="bi bi-heart"></i>
         <span>Favoritos</span>
@@ -20,17 +17,30 @@
         <i class="bi bi-cart-fill"></i>
         <span>Carrinho</span>
       </a>
-
-
-
-
     </div>
   </div>
 </nav>
-
-
-
 </div>
+</div>
+<div class="container-fluid bg-light navigation-bar">
+  <div class="container">
+    <nav class="navbar bg-light row my-0 py-0">
+      <nav class="navbar navbar-expand row bg-light col-sm-12 d-flex justify-content-center my-0">
+        <div class="container-fluid  d-flex justify-content-center">
+          <div class="collapse navbar-collapse  border-bottom-3   " id="navbarNavAltMarkup">
+            <div class="navbar-nav d-flex justify-content-center align-middle">
+              <a class="nav-link active py-2 flex-wrap" href="#">Home</a>
+              <a class="nav-link active py-2 flex-wrap" href="#">Ofertas quentes</a>
+              <a class="nav-link active py-2 flex-wrap" href="#">Categorias</a>
+              <a class="nav-link active py-2 flex-wrap" href="#">Laptops</a>
+              <a class="nav-link active py-2 flex-wrap" href="#">Smartphones</a>
+              <a class="nav-link active py-2 flex-wrap" href="#">Câmeras</a>
+              <a class="nav-link active py-2 flex-wrap" href="#">Acessórios</a>
+            </div>
+          </div>
+        </div>
+      </nav>
+  </div>
 </div>
 <!-- <ul>
   <li>
--- view/secoes/home.php
+++ view/secoes/home.php
@@ -1,79 +1,109 @@
 <main class="container">
-    <nav class="navbar bg-light row my-0 py-0">
-        <nav class="navbar navbar-expand row bg-light col-sm-12 d-flex justify-content-center my-0">
-            <div class="container-fluid  d-flex justify-content-center">
-                <div class="collapse navbar-collapse  border-bottom-3   " id="navbarNavAltMarkup">
-                    <div class="navbar-nav d-flex justify-content-center align-middle">
-                        <a class="nav-link active py-2 flex-wrap" href="#">Home</a>
-                        <a class="nav-link active py-2 flex-wrap" href="#">Ofertas quentes</a>
-                        <a class="nav-link active py-2 flex-wrap" href="#">Categorias</a>
-                        <a class="nav-link active py-2 flex-wrap" href="#">Laptops</a>
-                        <a class="nav-link active py-2 flex-wrap" href="#">Smartphones</a>
-                        <a class="nav-link active py-2 flex-wrap" href="#">Câmeras</a>
-                        <a class="nav-link active py-2 flex-wrap" href="#">Acessórios</a>
-                    </div>
-                </div>
-            </div>
-        </nav>
-        <section class="collections container-fluid row mb-5">
 
-            <div class="col-md-6 col-sm-12 col-lg-4 flex-wrap">
-                <div class="d-flex flex-column align-items-center">
-                    <div class="collections-efeito">
-                        <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
-                            laptop</p>
-                        <a href="#" class="links-collection px-4">Compre agora <i
-                                class="bi bi-arrow-right-circle-fill"></i></a>
-                    </div>
-                    <figure><img class="img-collections" src="src/images/laptop.webp" alt=""></figure>
+    <section class="collections container-fluid row mb-5">
+
+        <div class="col-md-6 col-sm-12 col-lg-4 flex-wrap">
+            <div class="d-flex flex-column align-items-center">
+                <div class="collections-efeito">
+                    <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
+                        laptop</p>
+                    <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
                 </div>
-            </div>
-            <div class="col-md-6 col-sm-12 col-lg-4 mx-auto flex-wrap">
-                <div class="d-flex flex-column align-items-center">
-                    <div class="collections-efeito">
-                        <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
-                            Headset</p>
-                        <a href="#" class="links-collection px-4">Compre agora <i
-                                class="bi bi-arrow-right-circle-fill"></i></a>
-                    </div>
-                    <figure><img class="img-collections" src="src/images/headset.webp" alt=""></figure>
-                </div>
-            </div>
-            <div class="col-md-6  col-sm-12 col-lg-4 collection px-4 flex-wrap">
-                <div class="d-flex flex-column align-items-center">
-                    <div class="collections-efeito">
-                        <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
-                            Câmera</p>
-                        <a href="#" class="links-collection px-4">Compre agora <i
-                                class="bi bi-arrow-right-circle-fill"></i></a>
-                    </div>
-                    <figure><img class="img-collections" src="src/images/camera.webp" alt=""></figure>
-                </div>
-            </div>
-        </section>
-        <div class="Container">
-            <div class="row border">
-                <h3 class="fs-2  col-sm-12 col-md-8 border">
-                    Novos produtos
-                </h3>
-                <nav class="navbar navbar-expand bg-light col-sm-12 col-md-4 border my-0">
-                    <div class="collapse navbar-collapse" id="navbarNav">
-                        <ul class="navbar-nav border d-flex justify-content-center align-items-center">
-                            <li class="nav-item a">
-                                <a class=" novo-produto-bar" aria-current="page" href="#">Laptops</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link novo-produto-bar active" href="#">Smartphones</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link novo-produto-bar active " href="#">Câmeras</a>
-                            </li>
-                            <li class="nav-item ">
-                                <a class="nav-link novo-produto-bar active ">Acessorios</a>
-                            </li>
-                        </ul>
-                    </div>
+                <figure><img class="img-collections" src="src/images/laptop.webp" alt=""></figure>
             </div>
         </div>
-        
+        <div class="col-md-6 col-sm-12 col-lg-4 mx-auto flex-wrap">
+            <div class="d-flex flex-column align-items-center">
+                <div class="collections-efeito mx-3">
+                    <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
+                        Headset</p>
+                    <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
+                </div>
+                <figure><img class="img-collections" src="src/images/headset.webp" alt=""></figure>
+            </div>
+        </div>
+        <div class="col-md-6  col-sm-12 col-lg-4 collection px-4 flex-wrap">
+            <div class="d-flex flex-column align-items-center">
+                <div class="collections-efeito">
+                    <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
+                        Câmera</p>
+                    <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
+                </div>
+                <figure><img class="img-collections" src="src/images/camera.webp" alt=""></figure>
+            </div>
+        </div>
+    </section>
+    <div class="Container">
+        <div class="row border">
+            <h3 class="fs-2  col-sm-12 col-md-8 border">
+                Novos produtos
+            </h3>
+            <nav class="navbar navbar-expand bg-light col-sm-12 col-md-4 border my-0">
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav border d-flex justify-content-center align-items-center">
+                        <li class="nav-item a">
+                            <a class=" novo-produto-bar" aria-current="page" href="#">Laptops</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link novo-produto-bar active" href="#">Smartphones</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link novo-produto-bar active " href="#">Câmeras</a>
+                        </li>
+                        <li class="nav-item ">
+                            <a class="nav-link novo-produto-bar active ">Acessorios</a>
+                        </li>
+                    </ul>
+                </div>
+        </div>
+    </div>
+    <section class="container ">
+        <div class="row  py-5">
+            <div class=" col-sm-12 col-md-6 col-lg-3 card-produtos ">
+                <figure class="card-produto-img">
+
+                    <img src="src/images/Img_Produtos/note12.jpg" alt="">
+                </figure>
+                <div class="card-body  d-flex  flex-column">
+                    <span>Categoria</span>
+                    <span>preço</span>
+                </div>
+            </div>
+            <div class=" col-sm-12 col-md-6 col-lg-3 card-produtos ">
+                <figure class="card-produto-img">
+
+                    <img src="src/images/Img_Produtos/note12.jpg" alt="">
+                </figure>
+                <div class="card-body  d-flex  flex-column">
+                    <span>Categoria</span>
+                    <span>preço</span>
+                </div>
+            </div>
+            <div class=" col-sm-12 col-md-6 col-lg-3 card-produtos ">
+                <figure class="card-produto-img">
+
+                    <img src="src/images/Img_Produtos/note12.jpg" alt="">
+                </figure>
+                <div class="card-body  d-flex  flex-column">
+                    <span>Categoria</span>
+                    <span>preço</span>
+                </div>
+            </div>
+            <div class=" col-sm-12 col-md-6 col-lg-3 card-produtos">
+                <figure class="card-produto-img">
+
+                    <img src="src/images/Img_Produtos/note12.jpg" alt="">
+                </figure>
+                <div class="card-body  d-flex  flex-column">
+                    <span>Categoria</span>
+                    <span>preço</span>
+                </div>
+                <button class="btn">teste</button>
+
+            </div>
+        </div>
+
+
+    </section>
+
 </main>
