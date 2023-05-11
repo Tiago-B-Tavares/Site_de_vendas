97a28ea1a1de5fb308d320118223af41fae2ed1a
Tiago-B-Tavares <tiagobritot2016@gmail.com>
2023-05-10 22:01:14 -0300

    Grid de produtos adicionado

--- view/secoes/home.php
+++ view/secoes/home.php
@@ -5,9 +5,10 @@
         <div class="col-md-6 col-sm-12 col-lg-4 flex-wrap">
             <div class="d-flex flex-column align-items-center">
                 <div class="collections-efeito">
-                    <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
-                        laptop</p>
-                    <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
+                    <p class="text-collection text-light text-start fw-bold py-3 px-4 text-wrap "3Coleção
+         laptop</p>
+                    <a href="#" class="links-collection px-4">Compre agora <i
+                            class="bi bi-arrow-right-circle-fill"></i></a>
                 </div>
                 <figure><img class="img-collections" src="src/images/laptop.webp" alt=""></figure>
             </div>
@@ -15,9 +16,10 @@
         <div class="col-md-6 col-sm-12 col-lg-4 mx-auto flex-wrap">
             <div class="d-flex flex-column align-items-center">
                 <div class="collections-efeito mx-3">
-                    <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
-                        Headset</p>
-                    <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
+                    <p class="text-collection text-light text-start fw-bold py-3 px-4 text-wrap "3Coleção
+         Headset</p>
+                    <a href="#" class="links-collection px-4">Compre agora <i
+                            class="bi bi-arrow-right-circle-fill"></i></a>
                 </div>
                 <figure><img class="img-collections" src="src/images/headset.webp" alt=""></figure>
             </div>
@@ -25,22 +27,23 @@
         <div class="col-md-6  col-sm-12 col-lg-4 collection px-4 flex-wrap">
             <div class="d-flex flex-column align-items-center">
                 <div class="collections-efeito">
-                    <p class="text-collection text-light text-start fw-bold py-5 px-4 text-wrap ">Coleção
-                        Câmera</p>
-                    <a href="#" class="links-collection px-4">Compre agora <i class="bi bi-arrow-right-circle-fill"></i></a>
+                    <p class="text-collection text-light text-start fw-bold py-3 px-4 text-wrap "3Coleção
+         Câmera</p>
+                    <a href="#" class="links-collection px-4">Compre agora <i
+                            class="bi bi-arrow-right-circle-fill"></i></a>
                 </div>
                 <figure><img class="img-collections" src="src/images/camera.webp" alt=""></figure>
             </div>
         </div>
     </section>
     <div class="Container">
-        <div class="row border">
-            <h3 class="fs-2  col-sm-12 col-md-8 border">
+        <div class="row ">
+            <h3 class="fs-2  col-sm-12 col-md-8 ">
                 Novos produtos
             </h3>
-            <nav class="navbar navbar-expand bg-light col-sm-12 col-md-4 border my-0">
+            <nav class="navbar navbar-expand  col-sm-12 col-md-4  my-0">
                 <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav border d-flex justify-content-center align-items-center">
+                    <ul class="navbar-nav  d-flex justify-content-center align-items-center">
                         <li class="nav-item a">
                             <a class=" novo-produto-bar" aria-current="page" href="#">Laptops</a>
                         </li>
@@ -58,52 +61,45 @@
         </div>
     </div>
     <section class="container ">
-        <div class="row  py-5">
-            <div class=" col-sm-12 col-md-6 col-lg-3 card-produtos ">
-                <figure class="card-produto-img">
-
-                    <img src="src/images/Img_Produtos/note12.jpg" alt="">
-                </figure>
-                <div class="card-body  d-flex  flex-column">
-                    <span>Categoria</span>
-                    <span>preço</span>
+        <div class="row">
+            <div class=" py-3 col-md-6  col-sm-12 col-lg-3 collection  flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="src/images/camera.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Descrição</h5>
+                        <p class="card-text">Preço</p>
+                    </div>
                 </div>
             </div>
-            <div class=" col-sm-12 col-md-6 col-lg-3 card-produtos ">
-                <figure class="card-produto-img">
-
-                    <img src="src/images/Img_Produtos/note12.jpg" alt="">
-                </figure>
-                <div class="card-body  d-flex  flex-column">
-                    <span>Categoria</span>
-                    <span>preço</span>
+            <div class="  py-3 col-md-6  col-sm-12 col-lg-3 collection  flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="src/images/camera.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Descrição</h5>
+                        <p class="card-text">Preço</p>
+                    </div>
                 </div>
             </div>
-            <div class=" col-sm-12 col-md-6 col-lg-3 card-produtos ">
-                <figure class="card-produto-img">
-
-                    <img src="src/images/Img_Produtos/note12.jpg" alt="">
-                </figure>
-                <div class="card-body  d-flex  flex-column">
-                    <span>Categoria</span>
-                    <span>preço</span>
+            <div class="  py-3 col-md-6  col-sm-12 col-lg-3 collection  flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="src/images/camera.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Descrição</h5>
+                        <p class="card-text">Preço</p>
+                    </div>
                 </div>
             </div>
-            <div class=" col-sm-12 col-md-6 col-lg-3 card-produtos">
-                <figure class="card-produto-img">
-
-                    <img src="src/images/Img_Produtos/note12.jpg" alt="">
-                </figure>
-                <div class="card-body  d-flex  flex-column">
-                    <span>Categoria</span>
-                    <span>preço</span>
+            <div class="  py-3 col-md-6  col-sm-12 col-lg-3 collection  flex">
+                <div class="card" style="width: 18rem;">
+                    <img src="src/images/camera.webp" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">Descrição</h5>
+                        <p class="card-text">Preço</p>
+                    </div>
                 </div>
-                <button class="btn">teste</button>
-
             </div>
+
         </div>
-
-
     </section>
 
 </main>
